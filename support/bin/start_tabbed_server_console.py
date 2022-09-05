78bc0e5e2bf9ad1e5cdc173e8bdadb7b7f585e44
Michael Lesser <mlesser@arizona.edu>
2022-09-05 09:07:46 -0700

    cleanup startup files

--- support/bin/start_tabbed_server_console.py
+++ support/bin/start_tabbed_server_console.py
@@ -6,7 +6,10 @@ import os
 import sys
 import time
 
-python = f"wt -w azcam -p AzCamServer --tabColor #990000 ipython --profile azcamserver --TerminalInteractiveShell.term_title_format=AzCamServer -i "
+poetry = "poetry run"
+poetry = ""
+
+python = f"wt -w azcam -p AzCamServer --title AzCamServer --tabColor #990000 {poetry} ipython --profile azcamserver -i "
 arguments = sys.argv[1:] if len(sys.argv) > 1 else [""]
 args = " ".join(arguments)
 cl = f"{python} -m azcam_itl.server -- {args}"
@@ -14,7 +17,7 @@ os.system(cl)
 
 time.sleep(1)
 
-python = f"wt -w azcam new-tab -p AzCamConsole --tabColor #000099 ipython --profile azcamconsole --TerminalInteractiveShell.term_title_format=AzCamConsole -i "
+python = f"wt -w azcam new-tab -p AzCamConsole --title AzCamConsole --tabColor #000099 {poetry} ipython --profile azcamconsole -i "
 arguments = sys.argv[1:] if len(sys.argv) > 1 else [""]
 args = " ".join(arguments)
 cl = f"{python} -m azcam_itl.console -- {args}"
