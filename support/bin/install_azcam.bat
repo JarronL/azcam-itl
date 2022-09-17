53a14db8aaa8824b4ed41262cf23a60081590f55
Michael Lesser <mlesser@arizona.edu>
2022-09-17 13:27:07 -0700

    cleanup

--- support/bin/install_azcam.bat
+++ support/bin/install_azcam.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Updating pip
-C:\data\venvs\azcam\Scripts\python.exe -m pip install --upgrade pip
+python.exe -m pip install --upgrade pip
 
 echo Installing in edit mode
 cd ..\..
