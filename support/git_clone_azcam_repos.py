dd2549b17684a21244a2fb69c4caf7b897988289
Michael Lesser <mlesser@arizona.edu>
2022-09-20 17:12:02 -0700

    clean

---
+++ support/git_clone_azcam_repos.py
@@ -0,0 +1,54 @@
+# clone repos from github
+
+import os
+import subprocess
+
+packages = [
+    "azcam",
+    "azcam-arc",
+    "azcam-archon",
+    "azcam-ascom",
+    "azcam-cryocon",
+    "azcam-ds9",
+    "azcam-ds9-winsupport",
+    "azcam-expstatus",
+    "azcam-fastapi",
+    "azcam-focus",
+    "azcam-imageserver",
+    "azcam-mag",
+    "azcam-monitor",
+    "azcam-observe",
+    "azcam-scripts",
+    "azcam-testers",
+    "azcam-tool",
+    "azcam-webstatus",
+    "azcam-webtools",
+    "azcam-itl",
+    "azcam-90prime",
+    "azcam-bcspec",
+    "azcam-bluechan",
+    "azcam-lbtguiders",
+    "azcam-mont4k",
+    "azcam-soguiders",
+    "azcam-vatt4k",
+    "azcam-vattspec",
+    "azcam-osu4k",
+    "azcam-pepsi",
+]
+github_root = "https://github.com/mplesser/"
+
+for repo in packages:
+    r = os.path.join(".",os.path.abspath(repo))
+    if os.path.exists(r):
+        print(f"{r} already exists")
+    else:
+        print(f"{r} will be cloned")
+
+    if 1:
+        try:
+            p = subprocess.Popen(f"git clone {github_root}{repo}.git", shell=False)
+            p.wait()
+        except Exception as e:
+            print(e)
+
+input("Press Enter to continue...")
