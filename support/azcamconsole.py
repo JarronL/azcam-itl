644cb79f24d887f23fa477f036759d8f93a9b34c
Michael Lesser <mlesser@arizona.edu>
2022-10-21 09:32:45 -0700

    update

--- support/azcamconsole.py
+++
@@ -1,9 +0,0 @@
-"""
-Start azcamconsole.
-
-Usage:
-  ipython --profile azcamconsole -i ~/azcam/azcam-itl/support/azcamconsole.py -- -system DESI
-  from azcam.cli import * # to load tools to command line namespace
-"""
-
-import azcam_itl.console
