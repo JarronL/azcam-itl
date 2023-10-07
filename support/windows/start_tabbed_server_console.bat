e0b74a27c045eab8d3395720e647d90b8c824281
Michael Lesser <mlesser@arizona.edu>
2023-10-07 12:26:33 -0700

    update

---
+++ support/windows/start_tabbed_server_console.bat
@@ -0,0 +1,9 @@
+@echo off
+
+Rem Start azcamserver and azcamconsole in different tabs with Windows Terminal.
+
+SET OPTIONS= --size 120,40 --pos 20,550
+
+wt -w azcam %OPTIONS% --title AzCamServer ipython --profile azcamserver -i -m azcam_itl.server -- %*
+
+wt -w azcam new-tab --title AzCamConsole ipython --profile azcamconsole -i -m azcam_itl.console -- %*
