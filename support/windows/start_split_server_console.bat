104475c133dea20c5a8964c329220ae2870dec71
Michael Lesser <mlesser@arizona.edu>
2023-10-11 14:55:45 -0700

    update

--- support/windows/start_split_server_console.bat
+++ support/windows/start_split_server_console.bat
@@ -2,10 +2,12 @@
 
 Rem Start azcamserver and azcamconsole in split Windows Terminal panes.
 
+Rem May need to run as admin on some systems.
+
 SET OPTIONS= --size 180,30 --pos 20,750
 
-wt -w azcam %OPTIONS% -p AzCamServer --title AzCamServer ipython --profile azcamserver -i -m azcam_itl.server -- %*
+wt -w azcam %OPTIONS% --title AzCamServer ipython --profile azcamserver -i -m azcam_itl.server -- %*
 
 timeout 2 > NUL
 
-wt -w azcam split-pane -V -p AzCamConsole --title AzCamConsole ipython --profile azcamconsole -i -m azcam_itl.console -- %*
+wt -w azcam split-pane -V --title AzCamConsole ipython --profile azcamconsole -i -m azcam_itl.console -- %*
--- support/windows/start_tabbed_server_console.bat
+++ support/windows/start_tabbed_server_console.bat
@@ -2,6 +2,8 @@
 
 Rem Start azcamserver and azcamconsole in different tabs with Windows Terminal.
 
+Rem May need to run as admin on some systems.
+
 SET OPTIONS= --size 120,40 --pos 20,550
 
 wt -w azcam %OPTIONS% --title AzCamServer ipython --profile azcamserver -i -m azcam_itl.server -- %*
