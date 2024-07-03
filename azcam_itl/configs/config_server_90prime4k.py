c43601e3d547092f700ce68e0ae7e7534e9a79c6
Michael Lesser <mlesser@arizona.edu>
2024-07-03 07:59:03 -0700

    update

--- azcam_itl/configs/config_server_90prime4k.py
+++ azcam_itl/configs/config_server_90prime4k.py
@@ -7,41 +7,31 @@ from azcam.tools.archon.exposure_archon import ExposureArchon
 
 from azcam_itl.detectors import detector_sta4850, detector_sta4850_2amps_top
 
-# ****************************************************************
 # controller
-# ****************************************************************
 controller = ControllerArchon()
 controller.camserver.port = 4242
 controller.camserver.host = "10.0.0.12"  # ITL3
 
-# ****************************************************************
 # temperature controller
-# ****************************************************************
 azcam.db.tools["tempcon"].temperature_ids = [2, 0]  # ITL3
 
-# ****************************************************************
 # exposure
-# ****************************************************************
 filetype = "MEF"
 exposure = ExposureArchon()
-exposure.fileconverter.set_detector_config(detector_sta4850_2amps_top)
 exposure.filetype = exposure.filetypes[filetype]
 exposure.image.filetype = exposure.filetypes[filetype]
 exposure.add_extensions = 0
-exposure.image.focalplane.gains = [
-    3.0,
-    3.0,
-]
-exposure.image.focalplane.rdnoises = [0.0, 0.0]
+if 1:
+    exposure.set_detpars(detector_sta4850_2amps_top)
+    exposure.fileconverter.set_detector_config(detector_sta4850_2amps_top)
+    exposure.image.focalplane.gains = 2 * [3.0]
+    exposure.image.focalplane.rdnoises = 2 * [0.0]
+else:
+    exposure.set_detpars(detector_sta4850)
+    exposure.fileconverter.set_detector_config(detector_sta4850)
+    exposure.image.focalplane.gains = 4 * [3.0]
+    exposure.image.focalplane.rdnoises = 4 * [0.0]
 
-
-# ****************************************************************
-# detector
-# ****************************************************************
-exposure.set_detpars(detector_sta4850_2amps_top)
-
-# ****************************************************************
 # system header
-# ****************************************************************
 template = os.path.join(azcam.db.datafolder, "templates", "fits_template_90prime4k.txt")
 system = System("ITL3", template)
