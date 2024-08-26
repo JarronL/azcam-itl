37a26c0c31a44ef0c30cc91e664fc4b46623d8aa
Michael Lesser <mlesser@arizona.edu>
2024-08-26 07:48:21 -0700

    update

--- azcam_itl/server.py
+++ azcam_itl/server.py
@@ -181,7 +181,7 @@ def setup():
     loadscripts(["azcam_itl.scripts.server"])
 
     # web server
-    if 0:
+    if 1:
         webserver = WebServer()
         webserver.port = 2403
         webserver.logcommands = 1
